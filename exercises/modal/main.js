302fe662ca5ffca4dc160a4276b4776f1be415c0
Marius Vičius <marius@vsbl.lt>
2022-08-06 12:05:37 +0300

    Update modal

--- exercises/modal/main.js
+++ exercises/modal/main.js
@@ -2,12 +2,20 @@ Vue.component('modal', {
     template: `
         <div class="modal is-active">
             <div class="modal-background"></div>
-            <div class="modal-content">
-                <div class="box">
+            <div class="modal-card">
+                <header class="modal-card-head">
+                    <p class="modal-card-title">
+                        <slot name="header"></slot>
+                    </p>
+                    <button class="delete" aria-label="close" @click="$emit('close')"></button>
+                </header>
+                <section class="modal-card-body">
                     <slot></slot>
-                </div>
+                </section>
+                <footer class="modal-card-foot">
+                    <slot name="footer"></slot>
+                </footer>
             </div>
-            <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
         </div>
     `
 })
